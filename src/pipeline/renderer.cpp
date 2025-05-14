89b7deed7aa9a8d261b014152fe27273a1c06bbd
LucaFranceschi01 <luca.franceschi01@estudiant.upf.edu>
2025-05-14 15:31:52 +0200

    Fix bug

--- src/pipeline/renderer.cpp
+++ src/pipeline/renderer.cpp
@@ -281,7 +281,7 @@ void SCN::Renderer::displaySceneSinglepass(SCN::Scene* scene, Camera* camera)
 		)
 	);
 
-	shader->setUniform("u_camera_position", camera->viewprojection_matrix.getTranslation());
+	shader->setUniform("u_camera_position", camera->eye);
 	shader->setUniform("u_inv_vp_mat", camera->inverse_viewprojection_matrix);
 	if (reflectance_model == PHONG) shader->setUniform("u_shininess", shininess);
 
